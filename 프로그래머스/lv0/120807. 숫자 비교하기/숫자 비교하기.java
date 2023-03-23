b1872830e9db4bd4f18d813fef645b6cc64b69e7
이동우 <110949256+eastmeet@users.noreply.github.com>
2023-03-23 14:00:18 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) {
-        boolean condition = (0 <= num1 && num1 <= 10_000 && 0 <= num2 && num2 <= 10_000);
-        
-        if (condition && num1 == num2) {
-            return 1;
-        } else {
-            return -1;
-        }
+        int answer = 0;
+        return answer;
     }
 }
