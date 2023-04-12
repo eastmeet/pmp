d08d7e19efdf5d27d3c2cefe147422080bec9524
이동우 <110949256+eastmeet@users.noreply.github.com>
2023-04-12 21:03:53 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.61 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,13 @@
+import java.lang.Math.*;
+class Solution {
+    public int solution(int n) {
+        if (n < 7) {
+            return 1;
+        }
+        
+        // 필요 피자수 = n / 7
+        return (int) Math.ceil(n / 7.0);
+        
+
+    }
+}
