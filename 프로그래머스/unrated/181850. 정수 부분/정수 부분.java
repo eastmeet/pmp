2d00897b3413cb6f358d1eabf8c5ce9f8d8de2c1
이동우 <110949256+eastmeet@users.noreply.github.com>
2023-04-21 21:59:56 +0900

    [unrated] Title: 정수 부분, Time: 0.22 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181850. 정수 부분/정수 부분.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+
+class Solution {
+    public int solution(double flo) {  
+        return (int)Math.floor(flo);//flow 정수부분
+        // 정수부분이란?
+            // 5.2이면 5를 리턴 소수점 다 버리자.
+        
+    }
+}
