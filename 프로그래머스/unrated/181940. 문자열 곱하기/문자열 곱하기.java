096ec9e761e289ed28abf3e9e9de4faddfdd984e
이동우 <110949256+eastmeet@users.noreply.github.com>
2023-04-28 20:38:09 +0900

    [unrated] Title: 문자열 곱하기, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,9 +1,5 @@
 class Solution {
     public String solution(String my_string, int k) {
-        String result = "";
-        for (int i = 0; i < k; i++) {
-            result += my_string;
-        }
-        return result;
+        return my_string.repeat(k);
     }
 }
