965a91d453b0da0dc5764c262a703667cd9b17d3
이동우 <110949256+eastmeet@users.noreply.github.com>
2023-05-29 23:26:10 +0900

    [unrated] Title: 코드 처리하기, Time: 0.04 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -1,34 +1,30 @@
 class Solution {
     public String solution(String code) {
-        // abc1abc1abc
+        StringBuilder ret = new StringBuilder();
         int mode = 0;
-        String ret = "";
         char[] c = code.toCharArray();
         for (int idx = 0; idx < code.length(); idx++) {
-            // mode가 0일때
             switch (mode) {
                 case 0: {
                     if (c[idx] != '1' && (idx % 2) == 0) {
-                        ret += c[idx];
-                    } else if (c[idx] == '1') {
+                        ret.append(c[idx]);
+                    }
+                    if (c[idx] == '1') {
                         mode = 1;
                     }
                     break;
                 }
                 case 1: {
                     if (c[idx] != '1' && (idx % 2) != 0) {
-                        ret += c[idx];
-                    } else if (c[idx] == '1') {
+                        ret.append(c[idx]);
+                    }
+                    if (c[idx] == '1') {
                         mode = 0;
                     }
                     break;
                 }
             }
         }
-        if (ret.isEmpty()) {
-            return "EMPTY";
-        }
-
-        return ret;
+        return ret.length() == 0 ? "EMPTY" : ret.toString();
     }
 }
