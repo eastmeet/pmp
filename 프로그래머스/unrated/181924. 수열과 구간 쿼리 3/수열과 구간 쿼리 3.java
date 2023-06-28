db3d939cfa5c008920b8b5a604409bf234cf20ef
이동우 <ys932184@gmail.com>
2023-06-28 10:05:12 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.06 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -1,19 +1,21 @@
-import java.util.Arrays;
 class Solution {
-    public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = Arrays.copyOf(arr, arr.length);
-        
-        // for-each
-        for (int[] query : queries) {
-            int i = query[0];
-            int j = query[1];
+    public static int[] solution(int[] arr, int[][] queries) {
+        // 순서바꾸기 배열 안에 배열 queries [[3,4],[1,2],[2,3]]
+        int rowOfLen = queries.length;
+        int columnOfLen = queries[0].length;
+        // queries에서 배열 하나씩 뽑아내기
+        for (int i = 0; i < rowOfLen; i++) {
+            for (int j = 0; j < columnOfLen - 1; j++) {
+                // 인덱스 값
+                int a = queries[i][j];
+                int b = queries[i][j + 1];
 
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-
-        }
-
+                // swap
+                int temp = arr[a];
+                arr[a] = arr[b];
+                arr[b] = temp;
+                }
+            }
         return arr;
     }
 }
