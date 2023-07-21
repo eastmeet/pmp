de02372e33d10ea4ff4dbafb56e50578aa806c89
Dongwoo Lee <ys932184@gmail.com>
2023-07-21 22:30:57 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 7.53 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,24 +1,22 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        List<Integer> result = new ArrayList<>();
-        // queries에서 querie를 하나 뽑는다.
-        for (int[] query : queries) {
-            // arr length에 맞춰 for문을 돌린다.    
-            int s = query[0];
-            int e = query[1];
-            int k = query[2];
-            
-            // arr 짜르기
-            int[] arrSlice = Arrays.copyOfRange(arr, s, e + 1);
-            
-            // 원소비교 및 최소값
-            int min = Arrays.stream(arrSlice).filter(x -> x > k).min().orElse(-1);
-            result.add(min);
+       int[] result = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+
+            result[i] = IntStream.rangeClosed(s, e)
+                                 .map(index -> arr[index])
+                                 .filter(value -> value > k)
+                                 .min()
+                                 .orElse(-1);
         }
         
-        return result.stream().mapToInt(x->x).toArray();
+        return result;
     }
-    
 }
