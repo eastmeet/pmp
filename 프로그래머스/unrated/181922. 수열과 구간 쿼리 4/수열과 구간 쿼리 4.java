3653e4da467b741ca7b1bfe0e5b721431f644579
Dongwoo Lee <ys932184@gmail.com>
2023-07-22 10:22:09 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 0.06 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -8,10 +8,10 @@ class Solution {
             int e = query[1];
             int k = query[2];
             
-            for (int i = s; i <= e; i++) {
-                if (i % k == 0) {
-                    arr[i] = arr[i] + 1;
-                }
+            //i가 k의 배수인지 판단
+            int start = s % k == 0 ? s : s + (k - s % k);
+            for (int i = start; i <= e; i += k) {
+                arr[i] += 1;
             }
         }
         return arr;
