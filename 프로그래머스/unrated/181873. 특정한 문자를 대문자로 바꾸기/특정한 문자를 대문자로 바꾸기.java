f6a558e6a62e90d470381641c6f929c4ad1bcd26
Dongwoo Lee <ys932184@gmail.com>
2023-09-17 23:28:56 +0900

    [unrated] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.08 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
+++ 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
@@ -1,15 +1,9 @@
 class Solution {
     public String solution(String my_string, String alp) {
-        String result = "";
         
-        for (char c : my_string.toCharArray()) {
-            if (c == alp.charAt(0)) {
-                c = Character.toUpperCase(c);
-            }
-            result += c;
-            
-        }
+        String x = alp.toUpperCase();
+        my_string = my_string.replaceAll(alp, x);
         
-        return result;
+        return my_string;
     }
 }
